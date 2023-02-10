1ae6a40e546e09cbb1c35f245a8dab041f6a84fc
Andreas Netteland <andreas.netteland@innovasjonnorge.no>
2023-02-10 11:53:39 +0100

    add app session

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import *
-import json
-from get_coords import get_coords
+from flask import Flask, request
+from get_coords import get_coords, normalize_ais_coords
+from ais import setup_ais_token, get_ais_data
+from sentinel import setup_sentinel_token
 from model.entry import predict
+from random import randint
 
 app = Flask(__name__)
-
+def setup_config():
+    app.config["ais_token"] = setup_ais_token()
+    sentinel_oauth, sentinel_token = setup_sentinel_token()
+    app.config["sentinel_token"] = sentinel_token
+    app.config["sentinel_oauth"] = sentinel_oauth
+setup_config()
 
 @app.route("/", methods=["GET"])
 def health_check():
@@ -12,11 +19,26 @@ def health_check():
     return "API Status ok"
 
 
+@app.route("/random-coord", methods=["GET"])
+def get_random_coord():
+    bboxes = get_coords()
+    rand = randint(0, len(bboxes)-1)
+    entry = [coord for coord in bboxes[rand]]
+    return normalize_ais_coords(entry)
+
+@app.route("/get-ais-data", methods=["POST"])
+def get_ais_data_endpoint():
+    coords = request.get_json()["coords"]
+    if not len(coords) == 5:
+        return "Bad formatting"
+    return get_ais_data(coords)
+
+
 @app.route("/do-predictions", methods=["GET"])
 def do_predictions():
     bboxes = get_coords()
     results = []
     for bbox in bboxes:
-        results.append((bbox), predict(bbox))
+        results.append(([coord for coord in bbox].append(bbox[0])), predict(bbox))
     # store results
     return "Done"
--- sentinel.py
+++ sentinel.py
@@ -1,5 +1,9 @@
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
+from flask import current_app
+from PIL import Image
+import io
+import numpy as np
 
 CLIENT_SECRET = "N&QBU3+>R}AJLqGQ(Z7?lTJ<.b8efeaR[p@xEWxw"
 CLIENT_ID = "d03af2a2-acaa-40c8-b824-7eb237eca07e"
@@ -21,19 +25,25 @@ function evaluatePixel(sample) {
 }
 """
 
-def setup_token():
+def setup_sentinel_token():
     client = BackendApplicationClient(client_id=CLIENT_ID)
     oauth = OAuth2Session(client=client)
 
     token = oauth.fetch_token(token_url='https://services.sentinel-hub.com/oauth/token',
                           client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    access_token = token["access_token"]
+    
+    return oauth, access_token
+
+def setup_sentinel_headers(token):
     url_request = 'https://services.sentinel-hub.com/api/v1/process'
     headers_request = {
-    "Authorization" : "Bearer %s" %token['access_token'],
+    "Authorization" : f"Bearer {token}",
     "Content-Type" : "application/json"
     }
 
     return url_request, headers_request
+
 
 def get_request_json(bbox):
     return {
@@ -74,7 +84,19 @@ def get_request_json(bbox):
 
 
 def make_request(bbox):
-    url, headers = setup_token()
+    access_token = current_app.config["sentinel_token"]
+    oauth = current_app.config["sentinel_oauth"]
+    url, headers = setup_sentinel_headers(access_token)
     payload = get_request_json(bbox)
     response = oauth.request("POST", url, headers=headers, json = payload)
-    return response.content
+    if response.status_code != 200:
+        oauth, access_token = setup_sentinel_token()
+        current_app.config["sentinel_token"] = access_token
+        current_app.config["sentinel_oauth"] = oauth
+        url, headers = setup_sentinel_headers(access_token)
+        response = oauth.request("POST", url, headers = headers, json = payload)
+    return response
+
+def get_sentinel_image(bbox):
+    response = make_request(bbox)
+    return np.array(Image.open(io.BytesIO(response.content)))
